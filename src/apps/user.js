a43ae121e868cf4e0daaf678f325988561c8017e
DEMDAR02 <99806033+DEMDAR02@users.noreply.github.com>
2023-05-01 14:57:57 +0700

    Add files via upload

---
+++ src/apps/user.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+export function usercomp (user){
+    return <>
+    <div>Пользователь: {user.user.name} {user.user.last_name}</div>
+    <div>Возраст: {user.user.age}</div>
+     <div>Питомец: {user.user.cat.name}</div> 
+     <div>Возраст питомца: {user.user.cat.age}</div>
+    </>;
+};
