9f9f59aae1e2ba57538504b3629865bca161e1fd
DEMDAR02 <99806033+DEMDAR02@users.noreply.github.com>
2023-05-11 09:27:53 +0700

    Add files via upload

---
+++ src/apps/Salaries.tsx
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import React from "react";
+interface SalariesProps {
+  user: {
+    salaries: [
+      salary: number,
+      from_date: number,
+    ]
+  },
+
+}
+class Salaries extends React.Component<SalariesProps> { 
+salary =0;
+from_date = 0;
+
+  render () {
+  return <table>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Salary</th>
+      <th>Date</th>
+    </tr>
+  </thead>
+  <tbody>
+    {salaries}
+  </tbody>
+</table>; 
+}
+  
+   }
+
+export default Salaries;
